a69feafbd783a58821ef5f5767d6dbe43372b114
park <deu04088@naver.com>
2025-07-24 17:46:14 +0900

    EDIT: nav code

--- function.js
+++ function.js
@@ -1,9 +1,13 @@
 function openDrawableMenu() {
-    document.getElementById("drawbleMenu").style.width="70%"
+    document.getElementById("drawableMenu").style.width="10%"
     document.getElementById("backdropMenu").style.display="block"
+    document.getElementById("openIcon").style.display = "none";
+    document.getElementById("menuCloseIcon").style.display = "inline";
 }
 
 function closeDrawableMenu() {
-    document.getElementById("drawbleMenu").style.width="0%"
+    document.getElementById("drawableMenu").style.width="0%"
     document.getElementById("backdropMenu").style.display="none"
+    document.getElementById("openIcon").style.display = "inline";
+    document.getElementById("menuCloseIcon").style.display = "none";
 }
