4264417ec21897a1650ec6d5b1de17c93ebcfb39
park <deu04088@naver.com>
2025-07-24 16:36:30 +0900

    ADD : nav, logo.png

---
+++ function.js
@@ -0,0 +1,9 @@
+function openDrawableMenu() {
+    document.getElementById("drawbleMenu").style.width="70%"
+    document.getElementById("backdropMenu").style.display="block"
+}
+
+function closeDrawableMenu() {
+    document.getElementById("drawbleMenu").style.width="0%"
+    document.getElementById("backdropMenu").style.display="none"
+}
