4103e9b0c65e5d206ddc3f70b8d2638fdc3d85ae
park <deu04088@naver.com>
2025-07-29 17:56:11 +0900

    EDIT: edit responsive web design

--- function.js
+++ function.js
@@ -1,10 +1,19 @@
 const sidebar = document.querySelector('.sidebar');
 const toggleBtn = document.getElementById('sidebar_toggle');
+const backdrop = document.querySelector('.backdrop');
 
 toggleBtn.addEventListener('click', function(event) {
     event.preventDefault();
     sidebar.classList.toggle('open');
+    backdrop.classList.toggle('open');
 });
+
+backdrop.addEventListener('click', function() {
+    sidebar.classList.remove('open');
+    backdrop.classList.remove('open');
+});
+
+//======================================================================================
 
 const search = document.querySelector('.search');
 const focusSearch = document.getElementById('search');
@@ -12,6 +21,12 @@ const focusSearch = document.getElementById('search');
 focusSearch.addEventListener('click', function() {
     search.classList.add('focus');
 });
-focusSearch.addEventListener('blue', function() {
-    search.classList.remove('focus');
+document.addEventListener('click', function(event) {
+    if(!search.contains(event.target)) {
+        search.classList.remove('focus');
+    }
 });
+//
+//focusSearch.addEventListener('click', function() {
+//    search.classList.remove('focus');
+//});
